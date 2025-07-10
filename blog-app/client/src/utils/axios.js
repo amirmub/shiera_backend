eddc988e49a285a0039791e2e4b0d18f3b9646a7
amirmub <amirmubarek01@gmail.com>
2025-07-10 08:43:39 +0300

    fix: update axios base URL

--- blog-app/client/src/utils/axios.js
+++ blog-app/client/src/utils/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
+//   baseURL: "http://localhost:4000",
+  baseURL: "https://shiera-backend-10.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
